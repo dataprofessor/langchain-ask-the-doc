04f3ed693c9c2fc9b2ea90b05f80f98e943f5c77
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-01 19:05:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,43 @@
 import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.qa import RetrievalQA
 from langchain.llms import OpenAI
-from langchain import PromptTemplate
 
 st.title('🦜🔗 Ask the Doc App')
-st.set_page_config(page_title="🦜🔗 Ask the Doc App")
+st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
+# File upload
 uploaded_file = st.file_uploader('Upload an article', type='txt')
-question = st.text_input('Ask something about the article', placeholder = 'Can you give me a short summary?', disabled = not uploaded_file)
+if uploaded_file is not None:
+    documents = [uploaded_file.read().decode('utf-8')]
+else:
+    documents = []
 
-def generate_response(topic):
-  llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
-  # Prompt
-  template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
-  prompt = PromptTemplate(input_variables = ['topic'], template = template)
-  prompt_query = prompt.format(topic=topic)
-  # Run LLM model
-  response = llm(prompt_query)
-  return st.info(response)
+# Split documents into chunks
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
 
+# Select embeddings
+embeddings = OpenAIEmbeddings()
+
+# Create vectorstore index
+db = Chroma.from_documents(texts, embeddings)
+
+# Create retriever interface
+retriever = db.as_retriever()
+
+# Create QA chain
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=retriever)
+
+# Form input and query
 with st.form('myform'):
-  topic_text = st.text_input('Enter prompt:', '')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(topic_text)
+    query_text = st.text_input('Enter your question:', '')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        response = qa.run(query_text)
+        st.info(response)
