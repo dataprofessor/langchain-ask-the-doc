0cfb41ed6822ebb8ef14ea59c2ead7585153fffc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-01 20:01:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ uploaded_file = st.file_uploader('Upload an article', type='txt')
 # Form input and query
 with st.form('myform'):
     query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled = not uploaded_file)
-    submitted = st.form_submit_button('Submit', disabled = not (uploaded_file AND openai_api_key))
+    submitted = st.form_submit_button('Submit', disabled = not (uploaded_file and openai_api_key))
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key!', icon='⚠')
     if submitted and openai_api_key.startswith('sk-'):
