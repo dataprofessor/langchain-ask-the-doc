27bf44bba49d734e5986352c5a61c96cceda7826
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-02 19:38:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', key='text')
 uploaded_file = st.file_uploader('Upload an article', type='txt')
 
 # Form input and query
+result = []
 with st.form('myform'):
     query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled = not uploaded_file)
     submitted = st.form_submit_button('Submit', disabled = not (uploaded_file and openai_api_key))
