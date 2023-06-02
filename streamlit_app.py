c2005c4c8692b2bdec4fece7d9b3d9ada83a1393
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-02 13:13:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 
+if 'OPENAI_API_KEY' not in st.session_state:
+    st.session_state['OPENAI_API_KEY'] = ''
+    
 def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
@@ -13,21 +16,23 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.create_documents(documents)
     # Select embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    #embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    embeddings = OpenAIEmbeddings()
     # Create vectorstore index
     db = Chroma.from_documents(texts, embeddings)
     # Create retriever interface
     retriever = db.as_retriever()
     # Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+    #qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+    qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type='stuff', retriever=retriever)
     response = qa.run(query_text)
     return response
 
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 
 st.title('🦜🔗 Ask the Doc App')
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
-#openai_api_key = st.secrets['OPENAI_API_KEY']
+#openai_api_key = st.sidebar.text_input('OpenAI API Key')
+st.session_state['OPENAI_API_KEY'] = st.sidebar.text_input('OpenAI API Key')
 
 if st.button('Clear API key'):
     del openai_api_key
