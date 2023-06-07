8dea60d65dfded5ddd880c5d2ee58a74cae667cb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-07 12:49:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     return response
 
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-
 st.title('🦜🔗 Ask the Doc App')
 
 # File upload
