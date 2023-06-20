476d527dd4d6972711a98e9842468acf7aadba80
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-06-21 06:14:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,20 @@ def generate_response(uploaded_file, openai_api_key, query_text):
     # Load document if file is uploaded
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
-    # Split documents into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.create_documents(documents)
-    # Select embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # Create a vectorstore from documents
-    db = Chroma.from_documents(texts, embeddings)
-    # Create retriever interface
-    retriever = db.as_retriever()
-    # Create QA chain
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    return qa.run(query_text)
+        # Split documents into chunks
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.create_documents(documents)
+        # Select embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        # Create a vectorstore from documents
+        db = Chroma.from_documents(texts, embeddings)
+        # Create retriever interface
+        retriever = db.as_retriever()
+        # Create QA chain
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        return qa.run(query_text)
+    else:
+        return st.warning('Please upload your document file to get started!')
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
